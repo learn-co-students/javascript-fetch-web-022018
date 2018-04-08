7d581ee1aa036f7f13cf69c5219b6a7f419132d8
sinclairstudy <sinclairstudy@gmail.com>
2018-04-08 19:35:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '9a6939ff85afad0fcb4008734a85d67161d28d31'
+fetch('https://api.github.com/s-kinch/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
